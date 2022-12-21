b652d95826fec183e632d309d0fb5a2f5a81750b
Vishnu-reddy123 <vishnu95532@gmail.com>
2022-12-21 22:52:54 +0530

    Retrieving employee payroll data

---
+++ src/main/java/org/example/EmployeePayrollService/EmployeePayrollService1.java
@@ -0,0 +1,27 @@
+package org.example.EmployeePayrollService;
+
+
+import java.sql.*;
+
+public class EmployeePayrollService1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll DB");
+        String jdbcurl = "jdbc:mysql://localhost:3306/payroll_service";
+        String username = "root";
+        String password = "***********";
+        Connection connection;
+        Statement statement;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(jdbcurl, username, password);
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee_payroll");
+            while (resultSet.next()) {
+                System.out.println("Name :" + resultSet.getString("Name"));
+                System.out.println("basic_pay :" + resultSet.getString("basic_pay"));
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.getStackTrace();
+        }
+    }
+}
