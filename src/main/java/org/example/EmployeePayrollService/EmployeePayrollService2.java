f52b953d1ee6692d627f1d423ee004515214f327
Vishnu-reddy123 <vishnu95532@gmail.com>
2022-12-21 23:05:23 +0530

    Updating the salary of employee and syncing to database

---
+++ src/main/java/org/example/EmployeePayrollService/EmployeePayrollService2.java
@@ -0,0 +1,37 @@
+package org.example.EmployeePayrollService;
+
+
+import java.sql.*;
+
+public class EmployeePayrollService2 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll DB");
+        String jdbcurl = "jdbc:mysql://localhost:3306/payroll_service";
+        String username = "root";
+        String password = "***********";
+        Connection connection;
+        Statement statement;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(jdbcurl, username, password);
+            statement = connection.createStatement();
+            //Updating record in the table
+            String sql1 = "UPDATE employee_payroll SET basic_pay ='3000000' WHERE Name ='Terrisa'";
+            //here addBatch is used to add individual statement to the batch.
+            statement.addBatch(sql1);
+            //to execute statement executeBatch used
+            statement.executeBatch();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee_payroll");
+            System.out.println("\tName\t\tbasic_pay");
+            System.out.println("-----------------------------");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(2) + "\t"
+                        + resultSet.getString(3));
+            }
+            connection.commit();
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.getStackTrace();
+        }
+    }
+}
---
+++ src/test/java/org/example/EmployeePayrollService/EmployeePayrollService2Test.java
@@ -0,0 +1,7 @@
+package org.example.EmployeePayrollService;
+
+import static org.junit.Assert.*;
+
+public class EmployeePayrollService2Test {
+
+}
